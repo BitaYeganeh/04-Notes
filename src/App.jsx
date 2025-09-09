00472f817d1f4d9186e8509a6f494735ae3e25e1
Bita Yeganeh <b.yeganeh85@gmail.com>
2025-09-09 12:19:54 +0300

    function parameters passed to map-method

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
 const App = (props) => {
   const {notes} = props
+  
+  const result = notes.map(note => note.id)
+  console.log(result)
+
+  const result2 = notes.map(note => note.content)
+  console.log(result2)
+
   return (
-    <div>
-      <h1>Notes</h1>
+    <>
+      <h1>Notes - hard coded</h1>
       <ul>
         
           <li>{notes[0].content}</li>
           <li>{notes[1].content}</li>
           <li>{notes[2].content}</li>      
       </ul>
-    </div>
-  
+      <hr />
+      <h1>Notes - map-metodilla</h1>
+      <ul>
+        {notes.map(note => 
+          <li key= {note.id}>
+            {note.content}</li>  
+        )}
+      </ul>
+      <hr />
+    </>
   )
+
 }
 export default App
