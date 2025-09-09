089363f3f21ef214fa483901af7aca817e6bf0b2
Bita Yeganeh <b.yeganeh85@gmail.com>
2025-09-09 10:50:13 +0300

    notes data is updated in main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-const App = () => {
+const App = (props) => {
+  const {notes} = props
   return (
-    <>
-      <p>Hello world</p>
-    </>
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        
+          <li>{notes[0].content}</li>
+          <li>{notes[1].content}</li>
+          <li>{notes[2].content}</li>      
+      </ul>
+    </div>
+  
   )
 }
-
 export default App
